f8b02f1cb633c147de07613a2c9afb5616b82482
Sparsh Rawlani <46356227+srawlani22@users.noreply.github.com>
2019-06-05 15:34:09 -0700

    updated the introToLystem file
    Draws the alphabet L from the commands given in the "instructions" variable.

--- tkinter/LSystems1.py
+++ tkinter/LSystems1.py
@@ -1,12 +1,22 @@
 import turtle
+# So python has a turtle library that helps us in using the defined methods of the library that further help us in drawing L.
 
 canvas = turtle.Screen()
+#opens the canvas/screen so that we can draw the alphabet
+
 draw = turtle.Turtle()
+# starts drawing the file
+
 draw.speed(1)
+# we set a drawing speed
+
+draw.left(90)
+# to make a straight L, I had to start the drawing from the Y-axis since it was set to X-axis by default
 
 instructions = "FFF+F+FF-F+F+FF"
+#instructions/L-system rules to draw an L
 
-for task in instructions:
+for task in instructions: #a for loop iterates through the instructions and draw an l based on the simple Lsystem rules which can be figured out by the if and else if statements below
     if task == "F":
         draw.forward(90)
     elif task == "+":
@@ -15,3 +25,4 @@ for task in instructions:
         draw.left(90)
 
 input("to exit")
+#a manual exit command so the GUI screen doesn't close as soon as the program finishes drawing the L.
