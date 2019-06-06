32569296bbb03d316a7623d1e10270115cb311ee
Sparsh Rawlani <srawlani22@my.whitworth.edu>
2019-06-06 08:45:31 -0700

    updates the LSystem python file

--- tkinter/LSystems1.py
+++ tkinter/LSystems1.py
@@ -5,14 +5,17 @@ draw = turtle.Turtle()
 draw.speed(1)
 draw.left(90)
 
-instructions = "FFF+F+FF-F+F+FF"
+# instructions = "FFF+F+FF-F+F+FF-fff"
+instructions= "fffff"
 
 for task in instructions:
-    if task == "F":
-        draw.forward(90)
-    elif task == "+":
-        draw.right(90)
-    elif task == "-":
-        draw.left(90)
+    # if task == "F":
+    #     draw.forward(90)
+    # elif task == "+":
+    #     draw.right(90)
+    # elif task == "-":
+    #     draw.left(90)
+    if task == "f":
+        draw.ondrag(90)
 
-input("to exit")
+input("")
