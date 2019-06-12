fdf6c3a93d0c0820ea3207d6b75482cdcc734787
Sparsh Rawlani <srawlani22@my.whitworth.edu>
2019-06-12 11:00:18 -0700

    added another ecursive file for tutorial

--- tkinter/recursionexamples/branches.py
+++ tkinter/recursionexamples/branches.py
@@ -1,29 +1,28 @@
+#referred from: https://interactivepython.org/runestone/static/pythonds/Recursion/pythondsintro-VisualizingRecursion.html
+
+
 import turtle 
 
-
-def tree(branchLength,branch):
-    #branch= turtle.Turtle()
-    if branchLength >10:
-        branch.forward(branchLength)
-        branch.right(20)
-        tree(branchLength-15 , branch)
-        branch.left(40)
-        tree(branchLength-15, branch)
-        branch.right(20)
-        branch.right(branchLength)
-
+def tree(branchLen,t):
+    if branchLen > 5:
+        t.forward(branchLen)
+        t.right(20)
+        tree(branchLen-15,t)
+        t.left(40)
+        tree(branchLen-15,t)
+        t.right(20)
+        t.backward(branchLen)
 
 def main():
     t = turtle.Turtle()
-    screen = turtle.Screen()
+    myWin = turtle.Screen()
     t.left(90)
     t.up()
     t.backward(100)
     t.down()
     t.color("green")
     tree(75,t)
-    screen.exitonclick()
+    myWin.exitonclick()
 
 main()
-
 
