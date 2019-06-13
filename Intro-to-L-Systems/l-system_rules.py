ebee60622e00c5241b072c0243a864d7aaaba46c
Sparsh Rawlani <srawlani22@my.whitworth.edu>
2019-06-13 11:20:12 -0700

    Renamed the folder

---
+++ Intro-to-L-Systems/l-system_rules.py
@@ -0,0 +1,31 @@
+def Lsystem_Rules(inputEx):
+    outputEx = ""
+    if inputEx == 'A':
+        outputEx = 'B'   # Rule 1
+    elif inputEx == 'B':
+        outputEx = 'AB'  # Rule 2
+    else:
+        outputEx = inputEx    # no rules apply so keep the character
+
+    return outputEx
+
+
+def processString(oldStr):
+    newstr = ""
+    for ch in oldStr:
+        newstr = newstr + Lsystem_Rules(ch)
+
+    return newstr
+
+
+def createLSystem(numIters,axiom):
+    startString = axiom
+    endString = ""
+    for i in range(numIters):
+        endString = processString(startString)
+        startString = endString
+
+    return endString
+
+
+print(createLSystem(5, "A"))
